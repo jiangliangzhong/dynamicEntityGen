fe67af77bf221eb75479cef2cff64c93f9e1b432
jiangliangzhong <1297551359@qq.com>
2019-10-23 20:19:28 +0800

    删除一些无意义类文件

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,16 @@
+import classgen.EntityClassGenUtil;
+import classgen.exception.CannotInitGenTool;
+import classgen.mapass.EntityClassInfo;
+import classgen.mapass.TableInfo;
+import classgen.mapass.TableWithEntityRelPool;
 import javassist.*;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 /**
  * 类{@code Application}:
@@ -11,31 +19,15 @@ import java.lang.reflect.InvocationTargetException;
  * @date 16:51 2019/10/18
  */
 public class Application {
-    public static void main(String[] args) throws ClassNotFoundException, NotFoundException, CannotCompileException, IOException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-        ClassPool classpool =ClassPool.getDefault();
-        CtClass ctClass = classpool.makeClass("entities.Student");
-        CtClass superTest = classpool.get("json.IJsonParse");
-        try {
-            ctClass.setSuperclass(superTest);
-        } catch (CannotCompileException e) {
-            e.printStackTrace();
-        }
-        try {
-            ctClass.addField(CtField.make("private int age;",ctClass));
-
-        } catch (CannotCompileException e) {
-            e.printStackTrace();
-        }
-        ctClass.addMethod(CtMethod.make("public void setAge(int age){this.age = age;}", ctClass));
-        ctClass.addMethod(CtMethod.make("public int getAge(){return this.age;}", ctClass));
-
-        byte[] byteArray = ctClass.toBytecode();
-        FileOutputStream output = new FileOutputStream(System.getProperty("user.dir")+"\\target\\classes\\Student.class");
-        output.write(byteArray);
-        output.close();
-
-        Class<?> clazz = Application.class.getClassLoader().loadClass("entities.Student");
-        Object obj = clazz.newInstance();
-        obj.getClass().getMethod("test").invoke(obj);
+    public static void main(String[] args) throws ClassNotFoundException, NotFoundException, CannotCompileException, IOException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, InterruptedException, ExecutionException, CannotInitGenTool {
+        EntityClassGenUtil.generateEntity();
+        Map<TableInfo, EntityClassInfo> entityClassInfoMap = TableWithEntityRelPool.getEntityClassInfoMap();
+        EntityClassInfo entityClassInfo = entityClassInfoMap.get(new TableInfo("student", "school"));
+        Class<?> clazz = entityClassInfo.getMyClassLoader().loadClass(entityClassInfo.getClassName());
+        Constructor constructor = clazz.getConstructor();
+        Object obj = constructor.newInstance();
+        obj.getClass().getMethod("setId", Integer.class).invoke(obj, 1);
+        System.out.println(obj.getClass().getMethod("getId").invoke(obj));
+        System.out.println("");
     }
 }
